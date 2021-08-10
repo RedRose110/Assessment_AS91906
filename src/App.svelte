9c0cd102396d0096fbacd1bdb397ab5433d1cb05
RedRose110 <>
2021-08-10 00:28:51 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  let name = ''
+  let form = ''
+
+</script>
+
+<label>
+  Name: 
+  <input bind:value={name}>
+  Class: 
+  <input bind:value={form}>
+</label>
+
+<h1>Welcome {name} from class </h1>
