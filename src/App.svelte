7cac183fb89a65f5756f123cdbe65834d39258c3
RedRose110 <60836241+RedRose110@users.noreply.github.com>
2021-08-10 01:43:26 +0000

    Added name, class, and a code that is similar to hello world. also added the first question with radio buttons for answers. have ytet added the code for check.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   let name = ''
   let form = ''
+  let check = ''
+  let answer = ''
 
 </script>
 
@@ -11,4 +13,19 @@
   <input bind:value={form}>
 </label>
 
-<h1>Welcome {name} from class </h1>
+<h1>Welcome {name} from class {form} to the Marvel quiz</h1>
+
+<p>
+  What is Black Widow's real name?
+</p>
+
+<label>
+	<input type=radio bind:group={answer} value ={true}>
+	"Natasha Romanoff"
+  <input type=radio bind:group={answer} value ={0}>
+	"Marina Duyunova"
+  <input type=radio bind:group={answer} value ={2}>
+	"Gregovich Kuznetsov"
+  <button on:click = {Check} id="check1">
+	check
+</label>
